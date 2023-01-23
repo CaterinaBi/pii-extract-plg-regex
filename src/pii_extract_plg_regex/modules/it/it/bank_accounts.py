ce300ed973d3da7e4457fab6e3551f67885304fc
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2023-01-23 16:42:09 +0000

    Creates docstring

--- src/pii_extract_plg_regex/modules/it/it/bank_accounts.py
+++ src/pii_extract_plg_regex/modules/it/it/bank_accounts.py
@@ -1,1 +1,4 @@
-
+'''
+IBAN (International Bank Account Number):
+  In Italy, IBANs consist of 27 digits and include the country code, check digits, bank code, bank branch code, national code, and bank account number.
+'''
