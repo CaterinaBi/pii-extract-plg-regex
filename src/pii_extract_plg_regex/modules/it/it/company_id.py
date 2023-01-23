6b329961fd1802a81273d60ba99c40ef040cd87d
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2023-01-23 16:44:24 +0000

    Creates docstring

--- src/pii_extract_plg_regex/modules/it/it/company_id.py
+++ src/pii_extract_plg_regex/modules/it/it/company_id.py
@@ -1,1 +1,4 @@
-
+'''
+Partita IVA (VAT number for companies):
+  The Partita IVA consists of 11 digits. The first 7 digits are a company identifier, the next 3 refer to the province of residence and the last is a check digit.
+'''
