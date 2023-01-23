b94bb2df2a04b0aa585bc11c40a52926ef74d31d
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2023-01-23 16:48:15 +0000

    Modifies docstring

--- src/pii_extract_plg_regex/modules/it/it/gov_id.py
+++ src/pii_extract_plg_regex/modules/it/it/gov_id.py
@@ -1,5 +1,13 @@
 '''
 Carta d'identità (identity card):
+
 Passaporto (passport):
+
+Codice fiscale (tax code for individuals):
+  An alphanumeric code of 16 characters used to identify individuals residing in Italy.
+  Non-individuals use the Partita IVA code (see `companies.py`).
+  The 16 digit number consists of three characters derived from the person's last name, three from the person's first name, 
+  five that hold information on the person's gender and birth date, four that represent the person's place of birth and one check digit.
+  
 Permesso di soggiorno per cittadini extracomunitari (VISA for people from extra-EU countries):
 '''
